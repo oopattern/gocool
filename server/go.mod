f1000fbcb58878f384654d89b41ba5c216486440
oopattern@126.com <oopattern@126.com>
2020-11-29 22:37:46 +0800

    add prometheus

--- server/go.mod
+++ server/go.mod
@@ -4,7 +4,9 @@ go 1.15
 
 require (
 	github.com/grpc-ecosystem/go-grpc-middleware v1.2.2
+	github.com/grpc-ecosystem/go-grpc-prometheus v1.2.0
 	github.com/oopattern/gocool v0.0.0-20201128102623-6d29e7602796
-	go.uber.org/zap v1.10.0
+	github.com/prometheus/client_golang v1.8.0 // indirect
+	go.uber.org/zap v1.13.0
 	google.golang.org/grpc v1.33.2
 )
--- server/server.go
+++ server/server.go
@@ -3,12 +3,24 @@ package server
 // ref: https://github.com/apssouza22/grpc-production-go/blob/master/server/server.go
 // ref: https://colobu.com/2017/04/17/dive-into-gRPC-interceptor/
 // ref: https://www.liwenzhou.com/posts/Go/zap/
+// ref: https://prometheus.io/
+// ref: https://programmaticponderings.com/tag/prometheus/
+// ref: https://medium.com/htc-research-engineering-blog/build-a-monitoring-dashboard-by-prometheus-grafana-741a7d949ec2
 import (
-	"net"
+	"fmt"
 	"log"
+	"net"
+	"net/http"
 	"google.golang.org/grpc"
+	"github.com/prometheus/client_golang/prometheus/promhttp"
 	grpc_middleware "github.com/grpc-ecosystem/go-grpc-middleware"
 	grpc_zap "github.com/grpc-ecosystem/go-grpc-middleware/logging/zap"
+	grpc_prometheus "github.com/grpc-ecosystem/go-grpc-prometheus"
+	// grpc_opentracing "github.com/grpc-ecosystem/go-grpc-middleware/tracing/opentracing"
+)
+
+var (
+	MetricsHttpPort = 9095
 )
 
 type GrpcServer interface {
@@ -30,13 +42,27 @@ func (s *grpcServer) Run() {
 }
 
 func NewServer(endpoint string) GrpcServer {
-	unaryOpt := grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(
-		grpc_zap.UnaryServerInterceptor(ZapLogger)))
+	// traceCfg := grpc_opentracing.UnaryServerInterceptor()
+	logCfg := grpc_zap.UnaryServerInterceptor(ZapLogger)
+	prometheusCfg := grpc_prometheus.UnaryServerInterceptor
+	unaryOpt := grpc.UnaryInterceptor(grpc_middleware.ChainUnaryServer(prometheusCfg, logCfg))
 
 	var opts []grpc.ServerOption
 	opts = append(opts, unaryOpt)
 	s := grpc.NewServer(opts...)
+	grpc_prometheus.Register(s)
+	grpc_prometheus.EnableHandlingTimeHistogram()
 
+	// Create a HTTP server for prometheus
+	httpServer := &http.Server{Addr: fmt.Sprintf("0.0.0.0:%d", MetricsHttpPort)}
+	http.Handle("/metrics", promhttp.Handler())
+	go func() {
+		if err := httpServer.ListenAndServe(); err != nil {
+			log.Fatal("Failed to start a http server")
+		}
+	}()
+
+	// Create a TCP  server
 	l, err := net.Listen("tcp", endpoint)
 	if err != nil {
 		log.Fatalf("failed to listen: +%v", err)
