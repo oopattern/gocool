542d0cba92024e30ed3eda3429639c4ae112d7e1
oopattern@126.com <oopattern@126.com>
2021-03-17 14:53:23 +0800

    add k8s script

--- script/consul.sh
+++ script/consul.sh
@@ -2,9 +2,9 @@
 # 1. consul安装
 
 # 2.1 consul启动, -dev为开发模式(非生产环境), 数据存储在内存中
-nohup consul agent -dev -client=0.0.0.0 -data-dir=/data/tools/consul/data -node=consul_sakulali -log-file=/data/tools/consul/log/consul.log &
+nohup consul agent -dev -client=0.0.0.0 -data-dir=/data/tools/consul/data -node=consul-sakulali -log-file=/data/tools/consul/log/consul.log &
 
 # 2.2 consul启动, 没有dev参数(生产环境), 数据存储在目录文件中
-nohup consul agent -client=0.0.0.0 -data-dir=/data/tools/consul/data -node=consul_sakulali -log-file=/data/tools/consul/log/consul.log -advertise=9.134.118.145 -bootstrap-expect=1 -server=true -join=9.134.118.145 -ui=true &
+nohup consul agent -client=0.0.0.0 -data-dir=/data/tools/consul/data -node=consul-sakulali -log-file=/data/tools/consul/log/consul.log -advertise=9.134.118.145 -bootstrap-expect=1 -server=true -join=9.134.118.145 -ui=true &
 
 ps aux | grep consul | grep -v grep | grep -v start_consul
---
+++ script/k8s.sh
@@ -0,0 +1,14 @@
+
+# 1. k8s安装
+https://github.com/ubuntu/microk8s
+https://microk8s.io/docs
+
+# 2. k8s启动, 生产环境
+ps aux | grep kube | grep -v grep
+
+# 3. 常用命令
+# microk8s.kubectl get all --all-namespaces // 查看所有资源
+# microk8s.kubectl -n kube-system get secret // 列出所有授权token
+# microk8s.kubectl -n kube-system describe secret kubernetes-dashboard-token-9hl6q // 查看dashboard的token
+# microk8s.kubectl cluster-info // 查看集群信息
+# microk8s dashboard-proxy // 运行代理dashboard, 在浏览器输入部署的机器外网ip, 然后在浏览器输入验证的token
